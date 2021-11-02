2ac0970d10627fab46f378ebf24518cb3b8cb38f
Maarten van Stam <maarten@vanstam.org>
2021-11-02 23:22:02 +0100

    Commit first web package

---
+++ src/softasinsoftware.com/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using softasinsoftware.com;
+
+using System;
+using System.Net.Http;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+});
+
+await builder.Build().RunAsync();
