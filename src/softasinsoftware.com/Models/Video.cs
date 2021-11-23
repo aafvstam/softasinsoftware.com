c7956cc71bece43c99a8f3c9af73ff90a1798605
Maarten van Stam <maarten@vanstam.org>
2021-11-23 22:55:45 +0100

    Added Mockup Video Page as a starter.

---
+++ src/softasinsoftware.com/Models/Video.cs
@@ -0,0 +1,24 @@
+﻿namespace softasinsoftware.com.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset ShowDate { get; set; }
+        public string Url { get; set; }
+
+        public string Provider { get; set; }
+        public string ProviderId { get; set; }
+
+        public bool HasTitle => !string.IsNullOrEmpty(Title);
+
+        // Tags
+        public bool IsNew => !IsInFuture && (DateTimeOffset.Now - ShowDate).TotalDays <= 7;
+        public bool IsInFuture => ShowDate > DateTimeOffset.Now;
+
+        // Do we need this?
+        public string ThumbnailUrl { get; set; }
+        public string LiveBroadcastContent { get; set; }
+    }
+}
---
+++ src/softasinsoftware.com/Pages/Index.razor.cs
@@ -0,0 +1,32 @@
+﻿using softasinsoftware.com.Models;
+
+namespace softasinsoftware.com.Pages
+{
+    public partial class Index
+    {
+        public IEnumerable<Video> VideoList { get; set; }
+
+        public Index()
+        {
+            //TODO: Create MockData and Service to collect videos from API
+            VideoList = Enumerable.Empty<Video>();
+
+            Video video = new Video();
+            {
+                video.Title = "Some Title";
+                video.Description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.";
+                video.ShowDate = DateTime.Now;
+                video.Url = "https://youtu.be/0H8yntZmq-s";
+                video.ThumbnailUrl = "https://i.ytimg.com/vi/0H8yntZmq-s/hqdefault_live.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDH7oj2IzdB4Nz3YbZcaiaX_Stsog";
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                VideoList = VideoList.Append(video);
+            }
+        }   
+
+        //TODO Take out hardcode URL
+        public string MoreShowsUrl => "https://www.youtube.com/playlist?list=PL06M1KyGksDAAjzsKLC0VNDCMmT7NcbTk";
+    }
+}
