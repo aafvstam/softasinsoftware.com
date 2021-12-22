9cd421ac6302aff2da4905bc3fcbd85244ff6513
Maarten van Stam <maarten@vanstam.org>
2021-12-22 10:32:25 +0100

    Changed API Uri to Production Uri, to do: Take out hardcoded URI

--- softasinsoftware.com/softasinsoftware.Web/Program.cs
+++ softasinsoftware.com/softasinsoftware.Web/Program.cs
@@ -7,7 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddHttpClient("softasinsoftware.API", client => client.BaseAddress = new Uri("https://localhost:7067/"));
+
+// todo take out hardcoded API URI
+builder.Services.AddHttpClient("softasinsoftware.API", client => client.BaseAddress = new Uri("https://softasinsoftwareapi.azurewebsites.net/"));
 
 builder.Services.AddMsalAuthentication(options =>
 {
