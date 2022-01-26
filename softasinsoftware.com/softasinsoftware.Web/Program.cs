93d0c358847027bf9fbce95f75bbcda0056f2a04
Maarten van Stam <maarten@vanstam.org>
2022-01-26 02:11:42 +0100

    Fixe API endpoint

--- softasinsoftware.com/softasinsoftware.Web/Program.cs
+++ softasinsoftware.com/softasinsoftware.Web/Program.cs
@@ -9,8 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // todo take out hardcoded API URI
-// builder.Services.AddHttpClient("softasinsoftware.API", client => client.BaseAddress = new Uri("https://softasinsoftwareapi.azurewebsites.net/"));
-builder.Services.AddHttpClient("softasinsoftware.API", client => client.BaseAddress = new Uri("https://localhost:7067/"));
+builder.Services.AddHttpClient("softasinsoftware.API", client => client.BaseAddress = new Uri("https://softasinsoftwareapi.azurewebsites.net/"));
+// builder.Services.AddHttpClient("softasinsoftware.API", client => client.BaseAddress = new Uri("https://localhost:7067/"));
 
 builder.Services.AddMsalAuthentication(options =>
 {
