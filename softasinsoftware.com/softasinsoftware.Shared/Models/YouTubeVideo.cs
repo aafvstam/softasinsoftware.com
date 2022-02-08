841a5c55a7c2e306048524df36bb6f9bdc0ee5d9
Maarten van Stam <maarten@vanstam.org>
2022-02-09 00:26:40 +0100

    Fixed Scoped css (bug in index.html, to do report to MS after repo) and added styiling to about page buttons.

--- softasinsoftware.com/softasinsoftware.Shared/Models/YouTubeVideo.cs
+++ softasinsoftware.com/softasinsoftware.Shared/Models/YouTubeVideo.cs
@@ -4,37 +4,38 @@ namespace softasinsoftware.Shared.Models
 {
     public class YouTubeVideo
     {
-        public string Id { get; set; }
-        public string Title { get; set; }
-        public string DisplayTitle { get; set; }
-        public string Topic { get; set; }
+        public string Id { get; set; } = String.Empty;
+        public string Title { get; set; } = String.Empty ;
+        public string DisplayTitle { get; set; } = String.Empty ;
+        public string Topic { get; set; } = String.Empty;
         
-        public bool HasDisplayTitle { get; set; }
-        public bool HasLinks { get; set; }
+        public bool HasDisplayTitle { get; set; } = default ;
+        public bool HasLinks { get; set; } = default ;
 
-        public string LinksUrl { get; set; }
-        public string Description { get; set; }
+        public string LinksUrl { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
 
         public DateTime? ScheduledStartTime { get; set; }
         public DateTime? ActualStartTime { get; set; }
         public DateTime? ActualEndTime { get; set; }
 
         // https://www.youtube.com/watch?v=a2uw7H_lWNA&list=PL06M1KyGksDAAjzsKLC0VNDCMmT7NcbTk&index=1&ab_channel=SoftAsInSoftware
-        public string Url { get; set; }
+        public string Url { get; set; } = String.Empty;
 
         // https://i.ytimg.com/vi/F_c35DzPJI8/hqdefault.jpg
-        public string ThumbnailUrl { get; set; }
+        public string ThumbnailUrl { get; set; } =String.Empty;
 
-        public string Category { get; set; }
+        public string Category { get; set; } = String.Empty;
 
         public DateTimeOffset ShowDate { get; set; }
 
-        public string Provider { get; set; }
-        public string ProviderId { get; set; }
+        public string Provider { get; set; } = String.Empty;
+        public string ProviderId { get; set; } = String.Empty;
 
         [JsonIgnore]
         public bool IsNew => !IsInFuture && 
                              !IsOnAir &&
+                             !(ScheduledStartTime == null) &&
                              (DateTime.UtcNow - ScheduledStartTime.Value).TotalDays <= 14;
         
         [JsonIgnore]
@@ -61,6 +62,6 @@ namespace softasinsoftware.Shared.Models
             return utcNow > scheduled.AddMinutes(-5) && utcNow < scheduled.AddHours(2);
         }
 
-        public string LiveBroadcastContent { get; set; }
+        public string LiveBroadcastContent { get; set; } = String.Empty;
     }
 }
