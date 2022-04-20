41832f5201aad7dd11c863ed7cd9a1423d428111
Maarten van Stam <maarten@vanstam.org>
2022-04-21 00:20:12 +0200

    Fix Images and remove two empty slots

--- softasinsoftware.com/softasinsoftware.API/Data/DbInitializer.cs
+++ softasinsoftware.com/softasinsoftware.API/Data/DbInitializer.cs
@@ -95,7 +95,7 @@ namespace softasinsoftware.API.Data
                     ShortURL = "https://amzn.to/37ugKfs",
                     URLAmazonNL = "https://www.amazon.nl/Sigma-18-50mm-F2-8-voor-Sony/dp/B09JVBB36L?crid=1QNF2AOD25KHZ&keywords=sigma+18-50mm+f2.8&qid=1650204039&sprefix=sigma%2Caps%2C61&sr=8-1&linkCode=ll1&tag=softasinsoftw-21&linkId=b9bbc299ef3a300f81f47269623ff29a&language=nl_NL&ref_=as_li_ss_tl",
                     ShortURLAmazonNL = "https://amzn.to/38QuDVD",
-                    Image = "71OBbXtQ6vL._AC_SL1500_.jpg"
+                    Image = "Sigma1850mm.jpg"
                 },
                 new GearItem
                 {
@@ -103,7 +103,7 @@ namespace softasinsoftware.API.Data
                     Description = "Behringer XENYX 1204USB Premium 12-Input 2/2-Bus Mixer with XENYX Mic Preamps and Compressors, British EQ and USB/Audio Interface",
                     URL = "https://www.amazon.com/gp/product/B00871VO5Y?ie=UTF8&linkCode=ll1&tag=softasinsoftw-20&linkId=f440b109e81b2540cc203a0d1e3bde61&language=en_US&ref_=as_li_ss_tl",
                     ShortURL = "https://amzn.to/3vrSh2z",
-                    Image = "51HtYlo84SL._AC_.jpg"
+                    Image = "BehringerXENYX.jpg"
                 },
                 new GearItem
                 {
@@ -111,7 +111,7 @@ namespace softasinsoftware.API.Data
                     Description = "Rode PSM1 Shock Mount For Podcaster, Procaster, PSA1, and DS1 Microphones",
                     URL = "https://www.amazon.com/gp/product/B000WA8KYG?ie=UTF8&linkCode=ll1&tag=softasinsoftw-20&linkId=dc88255c27fdaf5394923b620f6f8fcd&language=en_US&ref_=as_li_ss_tl",
                     ShortURL = "https://amzn.to/3rvrNfa",
-                    Image = "71nOuIkWaGL._AC_SL1500_.jpg"
+                    Image = "RodePSM1.jpg"
                 },
                 new GearItem
                 {
@@ -119,7 +119,7 @@ namespace softasinsoftware.API.Data
                     Description = "The dbx 286s is a full featured channel strip processor that delivers a studio quality microphone/instrument preamplifier and four processors that can be used independently or in any combination. Why mic up vocals and instruments through a noisy, blurry mixer? The sonically pristine dbx 286s mic preamp has all the features you need, including wide-ranging input gain control, switchable +48V phantom power, and an 80Hz high-pass filter to remove low frequency hum, rumble or wind. Use the patented dbx over Easy compressor to transparently smooth out uneven acoustic tracks or deliver that classic 'in your face' Vocal performance that only a dbx compressor can. Eliminate vocal sibilance and high frequency distortion from instruments such as cymbals with the frequency Tenable De-Esser. Fine-tune the Enhancer HF detail control to add sparkle and Crispness to your tracks and make adjustments to the lf detail control to add fullness and depth to vocals and bass instruments while cleaning up the muddy low midrange frequencies. The separate threshold and ratio controls on the expander/gate allow you to subtly reduce headphone leakage or radically gate noisy guitar amps. And, the dbx 286s offers a full compliment of metering and status LEDs to visually guide you to achieving the right sound.",
                     URL = "https://www.amazon.com/dbx-286s-Microphone-Channel-Processor/dp/B004LWH79A?crid=2NKYDNFGZUJVX&keywords=B004LWH79A&qid=1650205711&sprefix=b004lwh79a%2Caps%2C237&sr=8-1&th=1&linkCode=ll1&tag=softasinsoftw-20&linkId=57eb9fb2286df8e93a254c34ad2acc72&language=en_US&ref_=as_li_ss_tl",
                     ShortURL = "https://amzn.to/38KREt5",
-                    Image = "61GdC7ylERL._AC_SL1500_.jpg"
+                    Image = "DBX286s.jpg"
                 },
                 new GearItem
                 {
@@ -127,7 +127,7 @@ namespace softasinsoftware.API.Data
                     Description = "Connect it to Hue Hub (sold seperately) in order to control lights with your Hue App, Voice or Smart Home device. Compact design, full light experience. Create a vibrant ambiance with the Hue Play light bars. Choose from 16 million colors to experience different light effects. Lay it on the floor, let it stand on the cabinet, or mount it on the back of the TV and paint your wall with light. Sync your lights to music or movies using the Hue Sync app. This base kit provides 2 light points, 1 power supply unit, 2 table stands and 2 TV-mounting supports. Requires the Hue Hub (sold Separately) for the full Hue experience and to take advantage of voice activation. Purchase the Philips Hue Hub (Model: 458471). Search 'Philips Hue Hub' or 'B016H0QZ7I' to find this product on Amazon. Use the Hue dimmer switch, motion sensor, and other smart accessories to control the light bars.",
                     URL = "https://www.amazon.com/gp/product/B07GXB3S7Z?ie=UTF8&linkCode=ll1&tag=softasinsoftw-20&linkId=bd8e06cfd13cea785014d5e72300cc61&language=en_US&ref_=as_li_ss_tl",
                     ShortURL = "https://amzn.to/3vhLg44",
-                    Image = "519jIhifKoS._AC_SL1000_.jpg"
+                    Image = "PhilipsHuePlay.jpg"
                 },
                 new GearItem
                 {
@@ -135,7 +135,7 @@ namespace softasinsoftware.API.Data
                     Description = "The CORSAIR K55 RGB PRO Gaming Keyboard lights up your desktop with five-zone dynamic RGB backlighting and powers up your gameplay with six easy to set up dedicated macro keys. The K55 RGB PRO is certified for IP42 dust and spill-resistance to stand up to wear, tear, and more, with a detachable palm rest to ensure all-day comfort, win after win.",
                     URL = "https://www.amazon.com/gp/product/B08Y681W3X?ie=UTF8&linkCode=ll1&tag=softasinsoftw-20&linkId=89764a7f88b11fff42a10141bca78c66&language=en_US&ref_=as_li_ss_tl",
                     ShortURL = "https://amzn.to/3JNlDgT",
-                    Image = "81XDJ20fJ+L._AC_SL1500_.jpg"
+                    Image = "CorsairK55.jpg"
                 },
                 new GearItem
                 {
@@ -143,23 +143,7 @@ namespace softasinsoftware.API.Data
                     Description = "Work in comfort, stay in control. Work anywhere with this full-featured mouse, which delivers comfort in either hand and the precise control of BlueTrack Technology at a great value.",
                     URL = "https://www.amazon.com/Microsoft-Comfort-Mouse-4500-Lochness/dp/B00902BFGC?crid=2OS6RHMZ2AC0H&keywords=B00902BFGC&qid=1650205351&sprefix=b00902bfgc%2Caps%2C114&sr=8-1&th=1&linkCode=ll1&tag=softasinsoftw-20&linkId=c00f4860d45d1c6fcfe600dcb550e7a1&language=en_US&ref_=as_li_ss_tl",
                     ShortURL = "https://amzn.to/3McuA56",
-                    Image = "51wD-PlQ+DL._AC_SL1500_.jpg"
-                },
-                new GearItem
-                {
-                    Name = "",
-                    Description = "",
-                    URL = "",
-                    ShortURL = "",
-                    Image = ""
-                },
-                new GearItem
-                {
-                    Name = "",
-                    Description = "",
-                    URL = "",
-                    ShortURL = "",
-                    Image = ""
+                    Image = "ComfortMouse.jpg"
                 }
             };
 
