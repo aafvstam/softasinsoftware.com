e1545b1a9a9911bc0f81ee0f8177788810dac262
Maarten van Stam <aafvstam@hotmail.com>
2024-02-08 22:08:18 +0100

    Rebuilding to .NET 8.0.1 as it was failing ...

--- softasinsoftware.com/softasinsoftware.Web/Program.cs
+++ softasinsoftware.com/softasinsoftware.Web/Program.cs
@@ -1,6 +1,7 @@
 using Blazored.LocalStorage;
 
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using softasinsoftware.Web;
@@ -9,6 +10,7 @@ using softasinsoftware.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
